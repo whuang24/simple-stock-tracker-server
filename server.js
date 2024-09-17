03b6d8a7181f636b1a14316e456d79306b3f5f54
William Huang <whlxgx@gmail.com>
2024-09-16 22:46:46 -0700

    Installed Finnhub in npm and setting up basic finnhub requirements for server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+
+const finnhub = require('finnhub');
+
+const api_key = finnhub.ApiClient.instance.authentications['api_key'];
+api_key.apiKey = process.env.FINNHUB_API_KEY;
+const finnhubClient = new finnhub.DefaultApi()
+
