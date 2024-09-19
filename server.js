4ebf9e46f35f985ba477a1fcf255628073eb4468
William Huang <whlxgx@gmail.com>
2024-09-18 23:15:52 -0700

    Added stock data fetching functions to the server

--- server.js
+++ server.js
@@ -1,14 +1,55 @@
 const express = require("express")
 const app = express();
-const port = 3000;
+const port = 5000;
 const cors = require('cors')
 require('dotenv').config()
 
 import finnhubClient from './finnhub.js'
+import {db, graphDataCollection} from './firebase.js'
 
 app.use(express.urlencoded({extnded: true}));
 app.use(express.json());
 app.use(cors());
+
+async function syncWithDatabase(symbol, currTime, currPercent) {
+    const docRef = doc(db, "graphData", symbol)
+    await setDoc(docRef, {
+        graphData: {
+            [new Date().toISOString()]: {
+                time: currTime,
+                percentage: currPercent
+            }
+        }
+    }, {merge: true})
+}
+
+async function fetchData() {
+    for (let i = 0; i < props.watchlist.length; i++) {
+        const symbol = props.watchlist[i];
+
+        const currTime = new Date();
+
+        currTime.setTime(currTime.getTime() + currTime.getTimezoneOffset()*60*1000);
+
+        const currTimeNum = currTime.getTime();
+    
+        finnhubClient.quote(symbol, (error, data, response) => {
+            setWatchlistData(oldData => {
+                const newData = new Map(oldData)
+                newData.set(symbol, data)
+                return newData
+            })
+
+            if (marketStatus) {
+                syncWithDatabase(symbol, currTimeNum, data.dp);
+            }
+        })
+    }
+}
+
+app.get("/", cors(), async (req, res) => {
+    res.send("")
+})
 
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`)
