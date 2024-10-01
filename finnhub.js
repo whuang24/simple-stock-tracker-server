9b412fc262e5a47a45095f7bba5c2976967cc800
William Huang <whlxgx@gmail.com>
2024-09-30 21:37:08 -0700

    Added market status checking function to finnhub.js

--- finnhub.js
+++ finnhub.js
@@ -5,3 +5,9 @@ const finnhub = require('finnhub');
 const api_key = finnhub.ApiClient.instance.authentications['api_key'];
 api_key.apiKey = process.env.FINNHUB_API_KEY;
 export default finnhubClient = new finnhub.DefaultApi()
+
+export async function isMarketOpen() {
+    const response = await fetch(`https://finnhub.io/api/v1/stock/market-status?exchange=US&token=${API_KEY}`);
+    const data = await response.json();
+    return data.isOpen;
+}
