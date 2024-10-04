e3dadca07a8957a031a6319a918295d3036d59e3
William Huang <whlxgx@gmail.com>
2024-10-03 21:22:38 -0700

    Updated finnhub export

--- finnhub.js
+++ finnhub.js
@@ -4,7 +4,7 @@ const finnhub = require('finnhub');
 
 const api_key = finnhub.ApiClient.instance.authentications['api_key'];
 api_key.apiKey = process.env.FINNHUB_API_KEY;
-export default finnhubClient = new finnhub.DefaultApi()
+export const finnhubClient = new finnhub.DefaultApi()
 
 export async function isMarketOpen() {
     const response = await fetch(`https://finnhub.io/api/v1/stock/market-status?exchange=US&token=${API_KEY}`);
