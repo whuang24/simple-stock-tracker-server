69de887bb2c065512a6aacb4cd2e276a2308076a
William Huang <whlxgx@gmail.com>
2024-10-04 23:28:17 -0700

    Testing to see if removing graphData name from .env file will fix empty path on collection() issue

--- firebase.js
+++ firebase.js
@@ -9,11 +9,11 @@ const firebaseConfig = {
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
     projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID, 
     appId: process.env.FIREBASE_APP_ID
   };
   
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const graphDataCollection = collection(db, process.env.COLLECTION_NAME)
+export const graphDataCollection = collection(db, "graphData");
