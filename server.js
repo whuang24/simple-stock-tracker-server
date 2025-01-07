b797202706e1d9895452ce8b85f2f416a0de9d2c
William Huang <whlxgx@gmail.com>
2025-01-07 14:40:36 -0800

    Checking complete, data fetching functionality works as intended

--- server.js
+++ server.js
@@ -118,11 +118,9 @@ async function fetchData(currWatchlist) {
                     return;
                 }
 
-                syncWithDatabase(symbol, currTimeNum, data.dp);
-
-                // if (marketStatus && data.dp !== undefined) {
-                    
-                // }
+                if (marketStatus && data.dp !== undefined) {
+                    syncWithDatabase(symbol, currTimeNum, data.dp);
+                }
             });
         } catch (error) {
             console.error(`Error in fetchData for ${symbol}:`, error);
@@ -137,16 +135,16 @@ function startInterval() {
         try {
             const currWatchlist = await getWatchlist();
 
-            // if (!currWatchlist || currWatchlist.length === 0) {
-            //     console.warn("Watchlist is empty. Skipping fetch operation.");
-            //     return;
-            // }
+            if (!currWatchlist || currWatchlist.length === 0) {
+                console.warn("Watchlist is empty. Skipping fetch operation.");
+                return;
+            }
 
-            // const marketStatus = await checkMarket();
-            // if (!marketStatus) {
-            //     console.log("Market is closed. Skipping data fetch.");
-            //     return;                
-            // }
+            const marketStatus = await checkMarket();
+            if (!marketStatus) {
+                console.log("Market is closed. Skipping data fetch.");
+                return;                
+            }
 
             await fetchData(currWatchlist);
             console.log("Interval task completed.");
